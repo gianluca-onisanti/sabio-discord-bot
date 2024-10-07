a504ae43820ad24848be5f870cc8c81aba2b3053
Gianluca Onisanti <gianluca.onisanti@gmail.com>
2024-10-07 15:07:19 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv'); 
+dotenv.config()
+
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+    ],
+});
+
+// Carregando os comandos
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Ocorreu um erro ao executar esse comando!', ephemeral: true });
+    }
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
